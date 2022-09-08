106326715e3707a20891c4ad739516d4234bc8fe
Luiz Siqueira <siqueiradw@gmail.com>
2022-09-08 13:50:57 -0300

    Project created

---
+++ backend/src/test/java/com/siqueira/dsmeta/DsmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siqueira.dsmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DsmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
